9c7951d1b4a13624dff7f01e383505c99da930a2
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-06-13 11:28:43 +0300

    Update ver1.py

--- celeste/ver1.py
+++ celeste/ver1.py
@@ -7,18 +7,22 @@ class App:
     def __init__(self) -> None:
         self.screen = pygame.display.set_mode(SCREEN_SIZE)
         self.clock = pygame.Clock()
-        self.is_running = None
+        self.is_running = False
+        self.dt = 0
+        self.events = []
 
     def run(self):
         self.is_running = True
         while self.is_running:
-            self.input()
+            self.handling_events()
             self.update()
             self.draw()
+            pygame.display.update()
             self.dt = self.clock.tick(60)
 
-    def input(self):
-        for event in pygame.event.get():
+    def handling_events(self):
+        self.events = pygame.event.get()
+        for event in self.events:
             if event.type == pygame.QUIT:
                 raise SystemExit
 
@@ -27,7 +31,6 @@ class App:
 
     def draw(self):
         self.screen.fill("black")
-        pygame.display.update()
 
 
 if __name__ == "__main__":
